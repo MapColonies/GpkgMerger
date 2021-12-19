13d13208d7be5b935849246d3ef3d7cbdd490868
shimoncohen <shimoncohen655@gmail.com>
2021-12-19 04:26:50 +0200

    chore: add configuration json

---
+++ Src/Utils/Configuration.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace GpkgMerger.Src.Utils
+{
+    public class Configuration
+    {
+        private static Configuration instance = null;
+
+        private IConfiguration config;
+
+        // From: https://stackoverflow.com/questions/27880433/using-iconfiguration-in-c-sharp-class-library
+        private Configuration()
+        {
+            // Get configurations
+            string basePath = System.AppContext.BaseDirectory;
+            config = new ConfigurationBuilder()
+                        .SetBasePath(basePath)
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .Build();
+        }
+
+        public string GetConfiguration(params string[] configPath)
+        {
+            var config = this.config.GetSection(configPath[0]);
+            for (int i = 1; i < configPath.Length - 1; i++)
+            {
+                config = this.config.GetSection(configPath[i]);
+            }
+            return config[configPath[configPath.Length - 1]];
+        }
+
+        public static Configuration Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Configuration();
+                }
+                return instance;
+            }
+        }
+    }
+}
