f422bda0bf0d3301400e6d2d37fa3bb183780141
shimoncohen <shimoncohen655@gmail.com>
2022-04-10 11:26:07 +0300

    chore: change path teim to better functionality

--- Src/Utils/PathUtils.cs
+++ Src/Utils/PathUtils.cs
@@ -4,12 +4,7 @@ namespace GpkgMerger.Src.Utils
     {
         public static string RemoveTrailingSlash(string path)
         {
-            // Remove / from the end of the path if exists
-            if (path.EndsWith('/'))
-            {
-                path = path.Remove(path.Length - 1);
-            }
-            return path;
+            return path.TrimEnd('/');
         }
     }
 }
