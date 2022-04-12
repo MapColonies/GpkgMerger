41f28c7b570b4d02be610cd811bfdf926e375913
shimoncohen <shimoncohen655@gmail.com>
2022-04-12 10:51:01 +0300

    feat: merge multiple sources

--- Program.cs
+++ Program.cs
@@ -9,72 +9,85 @@ namespace GpkgMerger
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
+            Stopwatch totalTimeStopWatch = new Stopwatch();
+            totalTimeStopWatch.Start();
+            TimeSpan ts;
 
-            // Require input of 2 paths (base and new gpkg) and wanted batch size
-            if (args.Length != 6)
+            // Require input of wanted batch size and 2 types and paths (base and new gpkg)
+            if (args.Length < 6)
             {
-                Console.WriteLine($"Usage: {args[0]} <base_type> <path_to_base_datasource> <new_type> <path_to_new_datasource> <batch_size>");
-                Console.WriteLine($"Example: {args[0]} gpkg area1.gpkg gpkg area2.gpkg 1000");
-                Console.WriteLine($"Example: {args[0]} s3 /path1/on/s3 s3 /path2/on/s3 1000");
+                Console.WriteLine($"Usage: {args[0]} <batch_size> <base_type> <path_to_base_datasource> <new_type> <path_to_new_datasource>");
+                Console.WriteLine($"Example: {args[0]} 1000 gpkg area1.gpkg gpkg area2.gpkg");
+                Console.WriteLine($"Example: {args[0]} 1000 s3 /path1/on/s3 s3 /path2/on/s3");
+                Console.WriteLine($"Example: {args[0]} 1000 s3 /path/on/s3 gpkg geo.gpkg");
                 return;
             }
 
-            string baseType = args[1];
-            string newType = args[3];
-            string basePath = args[2];
-            string newPath = args[4];
-            int batchSize = int.Parse(args[5]);
+            int batchSize = int.Parse(args[1]);
+            string baseType = args[2];
+            string basePath = args[3];
 
-            // char* mergeType = getMergeType();
-            // if (mergeType == "GPKG")
-            // {
-            //     File.Exists();
-            //     File.Exists();
-            //     startGpkgProccess(basePath, newPath, batchSize);
-            // }
-            // else
-            // {
-            //     Directory.Exists();
-            //     Directory.Exists();
-            //     startFolderProccess(basePath, newPath, batchSize);
-            // }
+            int sourceIndex = 4;
 
-            // Data baseData = new Gpkg(fullBasePath, 1000);
-            // Data newData = new Gpkg(fullNewPath, 1000);
-
-            Data baseData = Data.CreateDatasource(baseType, basePath, batchSize);
-            Data newData = Data.CreateDatasource(newType, newPath, batchSize);
-
-            if (!baseData.Exists())
+            while (args.Length > sourceIndex + 1)
             {
-                Console.WriteLine("Base data does not exist.");
-                return;
-            }
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
 
-            if (!newData.Exists())
-            {
-                Console.WriteLine("New data does not exist");
-                return;
-            }
+                string newType = args[sourceIndex];
+                string newPath = args[sourceIndex + 1];
 
-            bool validate = bool.Parse(Configuration.Instance.GetConfiguration("GENERAL", "validate"));
-            if (validate)
-            {
-                Src.Proccess.Validate(baseData, newData);
-            }
-            else
-            {
+                // Console.WriteLine($"type: {newType}, path: {newPath}");
+                // sourceIndex += 2;
+                // continue;
+
+                Data baseData = Data.CreateDatasource(baseType, basePath, batchSize);
+                Data newData = Data.CreateDatasource(newType, newPath, batchSize);
+
+                if (!baseData.Exists())
+                {
+                    Console.WriteLine("Base data does not exist.");
+                    return;
+                }
+
+                if (!newData.Exists())
+                {
+                    Console.WriteLine("New data does not exist");
+                    return;
+                }
+
                 Src.Proccess.Start(baseData, newData);
+
+                // Do some calculation.
+                stopWatch.Stop();
+
+                // Get the elapsed time as a TimeSpan value.
+                ts = stopWatch.Elapsed;
+                TimeUtils.PrintElapsedTime($"{newPath} merge runtime", ts);
+
+                bool validate = bool.Parse(Configuration.Instance.GetConfiguration("GENERAL", "validate"));
+                if (validate)
+                {
+                    // Reset stopwatch for validation time measure
+                    stopWatch.Reset();
+                    stopWatch.Start();
+
+                    Console.WriteLine("Validating merged datasources");
+                    Src.Proccess.Validate(baseData, newData);
+
+                    stopWatch.Stop();
+                    // Get the elapsed time as a TimeSpan value.
+                    ts = stopWatch.Elapsed;
+                    TimeUtils.PrintElapsedTime($"{newPath} validation time", ts);
+                }
+
+                // Move to next source
+                sourceIndex += 2;
             }
 
-            // Do some calculation.
-            stopWatch.Stop();
-
+            totalTimeStopWatch.Stop();
             // Get the elapsed time as a TimeSpan value.
-            TimeSpan ts = stopWatch.Elapsed;
-
+            ts = totalTimeStopWatch.Elapsed;
             TimeUtils.PrintElapsedTime("Total runtime", ts);
         }
     }
