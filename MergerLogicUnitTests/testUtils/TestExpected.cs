a14cbfcff61d7aca255fe1c42d60a649e150663e
Shahar s <cl-shaharshavit-rnd@users.noreply.github.com>
2022-07-19 07:52:12 +0300

    test: 1X1 convertor unit tests

---
+++ MergerLogicUnitTests/testUtils/TestExpected.cs
@@ -0,0 +1,14 @@
+﻿namespace MergerLogicUnitTests.testUtils
+{
+    public class TestExpected<TestDataType, ExpectedDataType>
+    {
+        public TestDataType TestData { get; set; }
+        public ExpectedDataType ExpectedData { get; set; }
+
+        public TestExpected(TestDataType testData, ExpectedDataType expectedData)
+        {
+            this.TestData = testData;
+            this.ExpectedData = expectedData;
+        }
+    }
+}
