49b8da680451fc62ccc4c473ef2b8a5cecca1309
shimoncohen <shimoncohen655@gmail.com>
2022-07-24 18:11:28 +0300

    chore: add task data and metadata

--- MergerService/Models/Task.cs
+++ MergerService/Models/Task.cs
@@ -1,11 +1,11 @@
 using MergerLogic.Batching;
-using MergerService.Utils;
-using System.Text.Json;
+using Newtonsoft.Json;
+using System.ComponentModel;
 using System.Text.Json.Serialization;
 
 namespace MergerService.Controllers
 {
-    public class MergeTask
+    public class MergeMetadata
     {
         [JsonInclude]
         public TileBounds[]? Batches { get; }
@@ -13,7 +13,7 @@ namespace MergerService.Controllers
         [JsonInclude]
         public Source[]? Sources { get; }
 
-        public MergeTask(TileBounds[] batches, Source[] sources)
+        public MergeMetadata(TileBounds[] batches, Source[] sources)
         {
             this.Batches = batches;
             this.Sources = sources;
@@ -42,6 +42,81 @@ namespace MergerService.Controllers
             {
                 bounds.Print();
             }
+        }
+    }
+
+    public class MergeTask
+    {
+        [JsonInclude]
+        public string Id { get; }
+
+        [JsonInclude]
+        public string Type { get; }
+
+        [JsonInclude]
+        public string Description { get; }
+
+        [JsonInclude]
+        public MergeMetadata Parameters { get; }
+
+        [JsonInclude]
+        public string Status { get; }
+
+        [DefaultValue(0)]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        [JsonInclude]
+        public int? Percentage { get; }
+
+        [JsonInclude]
+        public string Reason { get; }
+
+        [JsonInclude]
+        public int Attempts { get; }
+
+        [JsonInclude]
+        public string JobId { get; }
+
+        [JsonInclude]
+        public bool Resettable { get; }
+
+        [JsonInclude]
+        public DateTime Created { get; }
+
+        [JsonInclude]
+        public DateTime Updated { get; }
+
+        public MergeTask(string id, string type, string description, MergeMetadata parameters,
+                            string status, int? percentage, string reason, int attempts,
+                            string jobId, bool resettable, DateTime created, DateTime updated)
+        {
+            this.Id = id;
+            this.Type = type;
+            this.Description = description;
+            this.Parameters = parameters;
+            this.Status = status;
+            this.Percentage = percentage;
+            this.Reason = reason;
+            this.Attempts = attempts;
+            this.JobId = jobId;
+            this.Resettable = resettable;
+            this.Created = created;
+            this.Updated = updated;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Id: {this.Id}");
+            Console.WriteLine($"Type: {this.Type}");
+            Console.WriteLine($"Description: {this.Description}");
+            this.Parameters.Print();
+            Console.WriteLine($"Status: {this.Status}");
+            Console.WriteLine($"Percentage: {this.Percentage}");
+            Console.WriteLine($"Reason: {this.Reason}");
+            Console.WriteLine($"Attempts: {this.Attempts}");
+            Console.WriteLine($"JobId: {this.JobId}");
+            Console.WriteLine($"Resettable: {this.Resettable}");
+            Console.WriteLine($"Created: {this.Created}");
+            Console.WriteLine($"Updated: {this.Updated}");
         }
     }
 }
