b832d8624ec973ad4875bdc951d245d24d3e471c
Shahar s <shaharS@users.noreply.github.com>
2022-09-14 12:51:41 +0300

    chore: naming

--- MergerLogic/ImageProcessing/TileScaler.cs
+++ MergerLogic/ImageProcessing/TileScaler.cs
@@ -16,11 +16,10 @@ namespace MergerLogic.ImageProcessing
             //find source pixels range
             double tilePartX = targetCoords.X % scale;
             double tilePartY = scale - 1 - (targetCoords.Y % scale);// flip direction as tiles are LL and pixels are UL
-            double subTileSize = TILE_SIZE / (double)scale;
+            int subTileSize = TILE_SIZE / scale;
 
             int pixelX = (int)(tilePartX * subTileSize);
             int pixelY = (int)(tilePartY * subTileSize);
-            int srcSize = Math.Max((int)subTileSize, 1);
 
             var srcPixels = baseImage.GetPixels();
             MagickImage scaledImage;
@@ -28,23 +27,23 @@ namespace MergerLogic.ImageProcessing
             var colorType = baseImage.ColorType;
             baseImage.ColorType = baseImage.HasAlpha ? ColorType.TrueColorAlpha : ColorType.TrueColor; 
             baseImage.ColorSpace = ColorSpace.sRGB;
-            if (scale >= 256)
+            if (scale >= TILE_SIZE)
             {
                 var color = srcPixels.GetPixel(pixelX, pixelY).ToColor()!;
                 scaledImage = new MagickImage(color,TILE_SIZE,TILE_SIZE);
             }
             else
             {
-                int maxSrcX = pixelX + srcSize;
-                int maxSrcY = pixelY + srcSize;
+                int maxSrcX = pixelX + subTileSize;
+                int maxSrcY = pixelY + subTileSize;
 
                 int channels = baseImage.ChannelCount;
                 int byteCount = channels * TILE_SIZE * TILE_SIZE;
                 var pixels = new byte[byteCount];
 
                 int scaledChannels = scale * channels;
-                int maxl = TILE_SIZE * scaledChannels;
-                int lIncrement = TILE_SIZE * channels;
+                int maxRowOffset = TILE_SIZE * scaledChannels;
+                int pixelRowBytes = TILE_SIZE * channels;
 
                 //loop relevant source pixels
                 for (int i = pixelX; i < maxSrcX; i++)
@@ -55,11 +54,11 @@ namespace MergerLogic.ImageProcessing
                         var targetXStart = (i - pixelX) * scale;
                         var targetYStart = (j - pixelY) * scale;
                         var targetPixelIdxStart = (targetXStart + (TILE_SIZE * targetYStart)) * channels;
-                        for (int k = 0; k < scaledChannels; k += channels)
+                        for (int pixelColOffset = 0; pixelColOffset < scaledChannels; pixelColOffset += channels)
                         {
-                            for (int l = 0; l < maxl; l += lIncrement)
+                            for (int pixelRowOffset = 0; pixelRowOffset < maxRowOffset; pixelRowOffset += pixelRowBytes)
                             {
-                                int pixelIdx = targetPixelIdxStart + k + l;
+                                int pixelIdx = targetPixelIdxStart + pixelColOffset + pixelRowOffset;
                                 srcPixel!.CopyTo(pixels, pixelIdx); //copy all channels 
                             }
                         }
