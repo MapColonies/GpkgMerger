65f42473b5aff5812d81ed3aac9490f4dd380559
shimoncohen <shimoncohen655@gmail.com>
2022-11-15 13:32:57 +0200

    chore: use using statement instead of manually disposing of image in upscale

--- MergerLogic/ImageProcessing/TileScaler.cs
+++ MergerLogic/ImageProcessing/TileScaler.cs
@@ -11,87 +11,6 @@ namespace MergerLogic.ImageProcessing
 
         public MagickImage? Upscale(MagickImage baseImage, Tile baseTile, Coord targetCoords)
         {
-            int zoomLevelDiff = targetCoords.Z - baseTile.Z;
-            int scale = 1 << zoomLevelDiff;
-
-            //find source pixels range
-            double tilePartX = targetCoords.X % scale;
-            double tilePartY = scale - 1 - (targetCoords.Y % scale);// flip direction as tiles are LL and pixels are UL
-            int subTileSize = TILE_SIZE / scale;
-
-            int pixelX = (int)(tilePartX * subTileSize);
-            int pixelY = (int)(tilePartY * subTileSize);
-
-            var srcPixels = baseImage.GetPixels();
-            MagickImage scaledImage;
-            var colorSpace = baseImage.ColorSpace;
-            var colorType = baseImage.ColorType;
-            baseImage.ColorType = baseImage.HasAlpha ? ColorType.TrueColorAlpha : ColorType.TrueColor; 
-            baseImage.ColorSpace = ColorSpace.sRGB;
-            if (scale >= TILE_SIZE)
-            {
-                var color = srcPixels.GetPixel(pixelX, pixelY).ToColor()!;
-                scaledImage = new MagickImage(color,TILE_SIZE,TILE_SIZE);
-            }
-            else
-            {
-                int maxSrcX = pixelX + subTileSize;
-                int maxSrcY = pixelY + subTileSize;
-
-                int channels = baseImage.ChannelCount;
-                int byteCount = channels * TILE_SIZE * TILE_SIZE;
-                var pixels = new byte[byteCount];
-
-                int scaledChannels = scale * channels;
-                int maxRowOffset = TILE_SIZE * scaledChannels;
-                int pixelRowBytes = TILE_SIZE * channels;
-
-                //loop relevant source pixels
-                for (int i = pixelX; i < maxSrcX; i++)
-                {
-                    for (int j = pixelY; j < maxSrcY; j++)
-                    {
-                        var srcPixel = srcPixels.GetValue(i, j);
-                        var targetXStart = (i - pixelX) * scale;
-                        var targetYStart = (j - pixelY) * scale;
-                        var targetPixelIdxStart = (targetXStart + (TILE_SIZE * targetYStart)) * channels;
-                        for (int pixelColOffset = 0; pixelColOffset < scaledChannels; pixelColOffset += channels)
-                        {
-                            for (int pixelRowOffset = 0; pixelRowOffset < maxRowOffset; pixelRowOffset += pixelRowBytes)
-                            {
-                                int pixelIdx = targetPixelIdxStart + pixelColOffset + pixelRowOffset;
-                                srcPixel!.CopyTo(pixels, pixelIdx); //copy all channels 
-                            }
-                        }
-                    }
-                }
-
-                var set = new PixelReadSettings(TILE_SIZE, TILE_SIZE, StorageType.Char,
-                    channels == 4 ? PixelMapping.RGBA : PixelMapping.RGB);
-                scaledImage = new MagickImage(pixels, set);
-            }
-
-            scaledImage.Format = baseImage.Format;
-            scaledImage.HasAlpha = baseImage.HasAlpha;
-            scaledImage.ColorType = colorType;
-            scaledImage.ColorSpace = colorSpace;
-            // TODO: return null only when the one color is black or white
-            // return scaledImage.TotalColors > 1 ? scaledImage : null;
-            // return IsWhite(scaledImage) || IsFullyTransparent(scaledImage) ? null : scaledImage;
-            return ImageUtils.IsTransparent(scaledImage) ? null : scaledImage;
-        }
-
-        public Tile? Upscale(Tile tile, Coord targetCoords)
-        {
-            var tileBytes = tile.GetImageBytes();
-            MagickImage tileImage = new MagickImage(tileBytes);
-            MagickImage? upscale = this.Upscale(tileImage, tile, targetCoords);
-            tileImage.Dispose();
-            return upscale is null ? null : new Tile(targetCoords, upscale.ToByteArray());
-        }
-
-        public MagickImage UpscaleFix(MagickImage baseImage, Tile baseTile, Coord targetCoords)
-        {
             // Calculate scale diff
             int zoomLevelDiff = targetCoords.Z - baseTile.Z;
             int scale = 1 << zoomLevelDiff;
@@ -180,7 +99,19 @@ namespace MergerLogic.ImageProcessing
             scaledImage.HasAlpha = baseImage.HasAlpha;
             scaledImage.ColorType = colorType;
             scaledImage.ColorSpace = colorSpace;
-            return scaledImage;
+            return ImageUtils.IsTransparent(scaledImage) ? null : scaledImage;
+        }
+
+        public Tile? Upscale(Tile tile, Coord targetCoords)
+        {
+            MagickImage? upscale;
+            var tileBytes = tile.GetImageBytes();
+            
+            using(MagickImage tileImage = new MagickImage(tileBytes)) {
+                upscale = this.Upscale(tileImage, tile, targetCoords);
+            }
+
+            return upscale is null ? null : new Tile(targetCoords, upscale.ToByteArray());
         }
     }
 }
