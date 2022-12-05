4203b9150287e6c04e63edc04842d9158cea0f1e
Shlomi Koncha <cl_shlomikoncha@rnd-hub.com>
2022-12-05 13:50:52 +0200

    feat: threads support

--- MergerLogicUnitTests/DataTypes/WMTSTest.cs
+++ MergerLogicUnitTests/DataTypes/WMTSTest.cs
@@ -531,7 +531,7 @@ namespace MergerLogicUnitTests.DataTypes
 
             string testIdentifier = offset.ToString();
             wmtsSource.setBatchIdentifier(testIdentifier);
-            wmtsSource.GetNextBatch(out string batchIdentifier);
+            wmtsSource.GetNextBatch(out string batchIdentifier, out string? _, null, null);
             Assert.AreEqual(testIdentifier, batchIdentifier);
 
             this.VerifyAll();
@@ -588,11 +588,11 @@ namespace MergerLogicUnitTests.DataTypes
             Grid grid = isOneXOne ? Grid.OneXOne : Grid.TwoXOne;
             var wmtsSource = new WMTS(this._serviceProviderMock.Object, "test", batchSize, extent, grid, origin, 21, 0);
 
-            wmtsSource.GetNextBatch(out string batchIdentifier);
-            wmtsSource.GetNextBatch(out batchIdentifier);
+            wmtsSource.GetNextBatch(out string batchIdentifier, out string? _, null, null);
+            wmtsSource.GetNextBatch(out batchIdentifier, out string? _, null, null);
             Assert.AreNotEqual("0", batchIdentifier);
             wmtsSource.Reset();
-            wmtsSource.GetNextBatch(out batchIdentifier);
+            wmtsSource.GetNextBatch(out batchIdentifier, out string? _, null, null);
             Assert.AreEqual("0", batchIdentifier);
             this.VerifyAll();
         }
@@ -684,7 +684,7 @@ namespace MergerLogicUnitTests.DataTypes
             for (int i = 0; i < tileBatches.Count; i++)
             {
                 var exactedBatch = tileBatches[i];
-                var res = wmtsSource.GetNextBatch(out string batchIdentifier);
+                var res = wmtsSource.GetNextBatch(out string batchIdentifier, out string? _, null, null);
 
                 CollectionAssert.AreEqual(exactedBatch.ToArray(), res, comparer);
                 string expectedBatchId = Math.Min(i * batchSize, tiles.Length).ToString();
