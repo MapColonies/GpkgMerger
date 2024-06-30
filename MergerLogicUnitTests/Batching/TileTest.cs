61da9e56499e96f415180f8af27ebdd2a1caf591
Nitzan Morr <nitzan.morr@gmail.com>
2024-07-01 02:26:40 +0300

    test: fixed the tests that uses data row

--- MergerLogicUnitTests/Batching/TileTest.cs
+++ MergerLogicUnitTests/Batching/TileTest.cs
@@ -1,6 +1,8 @@
 using MergerLogic.Batching;
+using MergerLogicUnitTests.testUtils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 
@@ -14,32 +16,40 @@ namespace MergerLogicUnitTests.Clients
     {
         #region mocks
         private MockRepository _repository;
-        private byte[] _jpeg;
         #endregion
 
         [TestInitialize]
         public void beforeEach()
         {
             this._repository = new MockRepository(MockBehavior.Strict);
-            this._jpeg = File.ReadAllBytes("image.jpeg");
         }
 
         #region CreateTile
 
+        public static IEnumerable<object[]> GenCreateTileUnknownFormatParams()
+        {
+            yield return new object[] { null };
+            yield return new object[] { File.ReadAllBytes("image.gif") };
+        }
+
         [TestMethod]
         [TestCategory("CreateTile")]
-        [DataRow(null)]
-        [DataRow()]
+        [DynamicData(nameof(GenCreateTileUnknownFormatParams), DynamicDataSourceType.Method)]
         public void CreateTileWithUnknownDataFormatFails(byte[] data)
         {
             Assert.ThrowsException<ValidationException>(() => new Tile(0, 0, 0, data));
             this._repository.VerifyAll();
         }
 
+        public static IEnumerable<object[]> GenCreateTileParams()
+        {
+            yield return new object[] { File.ReadAllBytes("image.jpeg") };
+            yield return new object[] { File.ReadAllBytes("image.png") };
+        }
+
         [TestMethod]
         [TestCategory("CreateTile")]
-        [DataRow(new byte[] { 0xFF, 0xD8, 0xFF, 0xDB })]
-        [DataRow(new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A })]
+        [DynamicData(nameof(GenCreateTileParams), DynamicDataSourceType.Method)]
         public void CreateTile(byte[] data)
         {
             Tile tile = new Tile(0, 0, 0, data);
